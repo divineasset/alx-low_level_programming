8586a2dc6649e1c57555afb16fefd50389600909
BLESSING ALADI OKPACHU <blessingaladi@gmail.com>
2022-10-22 00:31:21 -0700

    done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of
+ *  Jack Bauer, starting from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	int q, i;
+
+	for (q = 0; q <= 23; q++)
+	{
+		for (i = 0; i <= 59; i++)
+		{
+			_putchar((q / 10) + '0');
+			_putchar((q % 10) + '0');
+			_putchar(':');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
