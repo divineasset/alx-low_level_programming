e2c247e2a232717d1b7722df07a8ed631bb45bf2
BLESSING ALADI OKPACHU <blessingaladi@gmail.com>
2022-10-22 00:15:14 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,23 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - a function that prints 10 times the alphabet
- *
- * Return: X10 a-z
+ * print_alphabet_x10 -> print the lower case alphabet
  */
+
 void print_alphabet_x10(void)
 {
-	int a, k;
+	int p;
+	int j;
 
-	for (a = 0; a <= 10; a++)
+	for (j = 0; j < 10; j++)
 	{
-		for (k = 'a'; k <= 'z'; k++)
-		{
-			_putchar(k);
-		}
-		_putchar('\n');
-	}
 
+		for (p = 'a'; p <= 'z'; p++)
+		{
+			_putchar(p);
+		}
+
+	_putchar('\n');
+
+	}
 }
