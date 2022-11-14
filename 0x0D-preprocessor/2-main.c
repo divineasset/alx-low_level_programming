1b0a3e56bcda35fbacb524192d976f344b77ea2c
divineasset <blessingaladi@gmail.com>
2022-11-14 06:52:16 -0800

    done with task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file the program compile from,
+ * followed by a new line
+ * Return: return void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
